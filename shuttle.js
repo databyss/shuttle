ef264c1f06c87cb2d8812213d9dcf5c96ca1973a
Andrew Henninger <databyss@gmail.com>
2012-03-06 10:10:33 -0500

    switched from setInteval to requestAnimationFrame

--- shuttle.js
+++ shuttle.js
@@ -277,8 +277,37 @@ $(function() {
 		clickDebug += '<tr><td>game</td><td>(' + Math.round(x) + ', ' + (c.height - Math.round(y)) + ')</td></tr></table>';
 	});
 
-	setInterval(gameLoop, 10);
+	(function animloop(){
+      requestAnimFrame(animloop);
+      gameLoop();
+    })();
 });
+
+//BEGIN RAF SHIM
+// reference: http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function() {
+	return	window.requestAnimationFrame       || 
+			window.webkitRequestAnimationFrame || 
+			window.mozRequestAnimationFrame    || 
+			window.oRequestAnimationFrame      || 
+			window.msRequestAnimationFrame     || 
+			function(callback){
+				window.setTimeout(callback, 1000 / 60);
+			};
+})();
+ 
+ 
+    // usage: 
+    // instead of setInterval(render, 16) ....
+ 
+    //(function animloop(){
+    //  requestAnimFrame(animloop);
+    //  render();
+    //})();
+    // place the rAF *before* the render() to assure as close to 
+    // 60fps with the setTimeout fallback.
+// END RAF SHIM
 
 // BEGIN ImageLoader Scripts
 // ImageLoader based on http://www.html5rocks.com/en/tutorials/games/assetmanager/
