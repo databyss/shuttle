5d369e12bc8e030976eaa56373ca5c9490273493
Andrew Henninger <databyss@gmail.com>
2012-03-06 09:35:50 -0500

    fix bug in imageLoader

--- shuttle.js
+++ shuttle.js
@@ -1,7 +1,8 @@
 var exitFlag = false;
 var lastUpdate = null;
-var bgImage;
+var bgImage = null;
 var c, ctx;
+var imageManager = new ImageLoader();
 
 var clickDebug =  'Click Debug:';
 clickDebug += '<table><tr><td>click</td><td>(0,0)</td></tr>';
@@ -15,7 +16,6 @@ var inputKeys = { // defines key codes used for input
 	quit: 27 // ESC
 }
 	
-
 // input state object
 var input = {
 	left: false,
@@ -102,8 +102,10 @@ var player = {
 		// draw player
 		//ctx.fillStyle = this.color;
 		//ctx.fillRect(this.pos.x, this.pos.y, this.width, this.height);
-		ctx.drawImage(this.image, this.pos.x, this.pos.y);					
-	}				 
+		if(player.image !== null) {
+			ctx.drawImage(this.image, this.pos.x, this.pos.y);
+		}
+	}
 }
 
 function setMapBG() {
@@ -111,7 +113,9 @@ function setMapBG() {
 	//ctx.clearRect(0, 0, c.width, c.height);
 	//ctx.fillStyle = '#ffffff';
 	//ctx.fillRect(0, 0, c.width, c.height);
-	ctx.drawImage(bgImage, 0, 0, 400, 400);	
+	if(bgImage !== null) {
+		ctx.drawImage(bgImage, 0, 0, 400, 400);
+	}
 }
 
 function handleKeyDown(evt) {
@@ -224,27 +228,38 @@ function gameLoop() {
 	lastUpdate = newUpdate;				 	
 }
 
-$(function() {
-	// on ready
+function loadImages() {
+	// preload images	
+	imageManager.queueDownload('images/tardis1.png');
+	imageManager.queueDownload('images/spacebg400x400.png');
+	
+	imageManager.downloadAll(function() {
+		bgImage = imageManager.getAsset('images/spacebg400x400.png');
+		player.image = imageManager.getAsset('images/tardis1.png');
+		player.width = player.image.width;
+		player.height = player.image.height;
+	});
+}
 
+
+function setupCanvas() {
 	// define graphics contexts
 	c = document.getElementById("gameCanvas");
 	ctx = c.getContext("2d");
-	
+
 	// canvas defaults
 	ctx.lineWidth = 1;
-	
-	// setup background image
-	bgImage = new Image();
-	bgImage.src = 'images/spacebg400x400.png';
 
-	// set player image
-	player.image = new Image();
-	player.image.onload = function() {
-		player.width = this.width;
-		player.height = this.height;
-	}
-	player.image.src = 'images/tardis1.png';
+	// flip image and translate down to fix coordinates
+	ctx.scale(1, -1); // flip over x axis
+	ctx.translate(0, -c.height); // move (0,0) to bottom left to match cartisian plane 
+	ctx.translate(0.5, 0.5); // offset for aliasing
+}
+
+$(function() {
+	// on ready
+	setupCanvas();
+	loadImages(); //TODO: async, need to wait for finish before moving on.
 	
 	// add listeners for keyboard input
 	window.addEventListener('keydown', handleKeyDown, true);
@@ -262,11 +277,53 @@ $(function() {
 		clickDebug += '<tr><td>game</td><td>(' + Math.round(x) + ', ' + (c.height - Math.round(y)) + ')</td></tr></table>';
 	});
 
-	// flip image and translate down to fix coordinates
-	ctx.scale(1, -1); // flip over x axis
-	ctx.translate(0, -c.height); // move (0,0) to bottom left to match cartisian plane 
-	ctx.translate(0.5, 0.5); // offset for aliasing
-
 	setInterval(gameLoop, 10);
 });
 
+// BEGIN ImageLoader Scripts
+// ImageLoader based on http://www.html5rocks.com/en/tutorials/games/assetmanager/
+function ImageLoader() {
+	this.successCount = 0;
+	this.errorCount = 0;
+	this.downloadQueue = [];
+	this.cache = {};
+}
+
+ImageLoader.prototype.queueDownload = function(path) {
+	this.downloadQueue.push(path);
+}
+
+ImageLoader.prototype.downloadAll = function(downloadCallback) {
+	if(this.downloadQueue.length === 0) {
+		downloadCallback();
+	} else {
+		for(var i = 0; i < this.downloadQueue.length; i++) {
+			var path = this.downloadQueue[i];
+			var img = new Image();
+			var that = this;
+			img.addEventListener("load", function() {
+				that.successCount += 1;
+				if(that.isDone()) {
+					downloadCallback();
+				}
+			}, false);
+			img.addEventListener("error", function() {
+				that.errorCount += 1;
+				if(that.isDone()) {
+					downloadCallback();
+				}
+			}, false);
+			img.src = path;
+			this.cache[path] = img;
+		}
+	}
+}
+
+ImageLoader.prototype.isDone = function() {
+	return(this.downloadQueue.length === (this.successCount + this.errorCount));
+}
+
+ImageLoader.prototype.getAsset = function(path) {
+	return this.cache[path];
+}
+// END ImageLoader Scripts
