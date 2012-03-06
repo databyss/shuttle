ef43f5282c7c48a787dcb07c449b836784485b08
Andrew Henninger <databyss@gmail.com>
2012-03-06 08:54:36 -0500

    imageLoader script to handle asset loading

---
+++ imageLoader.js
@@ -0,0 +1,45 @@
+// ImageLoader based on http://www.html5rocks.com/en/tutorials/games/assetmanager/
+function ImageLoader() {
+	this.successCount = 0;
+	this.errorCount = 0;
+	this.downloadQueue = [];
+	this.cache = {};
+}
+
+ImageLoader.prototype.queueDownload = function(path) {
+	this.downloadQueue.push(path);
+}
+
+ImageLoader.prototype.downloadAll = function(downloadCallback) {
+	if(this.downloadQueue.length === 0) {
+		downloadCallback();
+	} else {
+		for(var i = 0; i < this.downloadQueue; i++) {
+			var path = this.downloadQueue[i];
+			var img = new Image();
+			var that = this;
+			img.addEventListener("load", function() {
+				that.successCount += 1;
+				if(that.isDone()) {
+					downloadCallback();
+				}
+			}, false);
+			img.addEventListener("error", function() {
+				that.errorCount += 1;
+				if(that.isDone()) {
+					downloadCallback();
+				}
+			}, false);
+			img.src = path;
+			this.cache[path] = img;
+		}
+	}
+}
+
+ImageLoader.prototype.isDone = function() {
+	return(this.downloadQueue.length === (this.successCount + this.errorCount));
+}
+
+ImageLoader.prototype.getAsset = function(path) {
+	return this.cache[path];
+}
